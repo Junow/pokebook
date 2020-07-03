c15d9351bbfd5d7e98a60eebbeb5552569b2f58b
junow <taristmas@gmail.com>
2020-07-04 02:36:57 +0900

    Fix <div> to <header>
    semantic html 적용용

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import { fontSize } from 'GlobalStyle'
 
-export const Container = styled.div`
+export const Container = styled.header`
   display: flex;
   width: 100%;
   justify-content: center;
