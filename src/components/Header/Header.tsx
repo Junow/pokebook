f2cfd7b9b7c4041cc6f8844dee93f59d86f06f5f
junow <taristmas@gmail.com>
2020-08-17 00:47:07 +0900

    Modify Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as S from './Header.styled'
 
-const Header = () => (<S.Container>PokeBook</S.Container>)
+const Header = () => (<S.Container>PokeDex</S.Container>)
 
 export default Header
