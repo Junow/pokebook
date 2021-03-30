ef32137a889ac6da9993e25ba897f257faacd776
Tiago Relvao <tiago@mosaicsmartdata.com>
2021-03-30 15:21:12 +0100

    remove sprites

--- src/components/PokeByUrl/PokeByUrl.tsx
+++ src/components/PokeByUrl/PokeByUrl.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import useSwr from 'swr'
 import { ResponseByNameAPI } from 'types/response'
 import { fetcher } from 'utils/request'
 import { Span } from 'components/Simple'
@@ -11,45 +10,54 @@ import * as S from './PokeByUrl.styled'
 interface Props{
   url: string
 }
-const PokeByUrl: React.FC<Props> = ({ url }) => {
-  const {
-    data, error,
-  } = useSwr<ResponseByNameAPI>(url, fetcher)
 
-  const [imgUrls, setImgUrls] = React.useState<string[]>([])
+interface State {
+  data: ResponseByNameAPI | undefined;
+  error: any;
+}
 
-  React.useEffect(() => {
-    if (data) {
-      const { sprites } = data
-      const spriteArr: string[] = []
-      Object.keys(sprites).forEach((key) => {
-        const value = sprites[key]
-        if (value === null) return
-        if (value === undefined) return
-        spriteArr.push(value as string)
-      })
-      setImgUrls(spriteArr)
+export class PokeByUrl extends React.PureComponent<Props, State> {
+  state: State = {
+    data: undefined,
+    error: undefined
+  };
+
+  componentDidMount() {
+    const { url } = this.props;
+    fetcher(url).then((data) => {
+      this.setState({data})
+    })
+  }
+
+  componentDidUpdate() {
+    const { url } = this.props;
+    fetcher(url).then((data) => {
+      this.state.data = data;
+    })
+  }
+
+  render () {
+    const { error, data } = this.state;
+
+    if (error) {
+      return <div>error...</div>
     }
-  }, [data])
-
-  if (error) {
-    return <div>error...</div>
+  
+    if (!data) {
+      return <LoaderSpinner/>
+    }
+  
+    return (
+      <S.Conatiner>
+        <div><Span size='xxxs'>No.</Span> {data.id}</div>
+        <LazyImage src={data.sprites.front_default} />
+        <div>name: {data.name}</div>
+        <div>height: {data.height}</div>
+        <div>weight: {data.weight}</div>
+        <div>abilities: {data.abilities.map((v) => v.ability.name).join(', ')}</div>
+      </S.Conatiner>
+    )
   }
-
-  if (!data) {
-    return <LoaderSpinner/>
-  }
-
-  return (
-    <S.Conatiner>
-      <div><Span size='xxxs'>No.</Span> {data.id}</div>
-      <LazyImage src={data.sprites.front_default} />
-      <div>name: {data.name}</div>
-      <div>height: {data.height}</div>
-      <div>weight: {data.weight}</div>
-      <div>abilities: {data.abilities.map((v) => v.ability.name).join(', ')}</div>
-    </S.Conatiner>
-  )
 }
 
 export default PokeByUrl
